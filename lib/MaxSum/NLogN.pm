1b3ad96e33f46b66d46736ce4ed98b77cee76a42
André Pontes <andrepontes@gmail.com>
2017-02-11 18:38:57 -0200

    NlogN is now a hybrid solution: can call squared solution for the base of the searching tree

--- lib/MaxSum/NLogN.pm
+++ lib/MaxSum/NLogN.pm
@@ -1,6 +1,7 @@
 package MaxSum::NLogN;
 
 use Exporter qw(import);
+use MaxSum::Squared qw(find_max_sub_array_part);
 
 our @EXPORT_OK = qw(find_max_sub_array);
 
@@ -48,30 +49,35 @@ sub find_max_sub_array_rec {
         return ( $low, $high, $a[$low] );
     }
     else {
-        my $mid = int( ( $low + $high ) / 2 );
-        my @solution_left  = find_max_sub_array_rec( \@a, $low,     $mid );
-        my @solution_right = find_max_sub_array_rec( \@a, $mid + 1, $high );
-        my @solution_cross = find_max_crossing_sub_array( \@a, $low, $mid, $high );
-        my $left_low   = $solution_left[0];
-        my $left_high  = $solution_left[1];
-        my $left_sum   = $solution_left[2];
-        my $right_low  = $solution_right[0];
-        my $right_high = $solution_right[1];
-        my $right_sum  = $solution_right[2];
-        my $cross_low  = $solution_cross[0];
-        my $cross_high = $solution_cross[1];
-        my $cross_sum  = $solution_cross[2];
+        if (($high - $low) < 120) {
+          return MaxSum::Squared::find_max_sub_array_part($a_ref, $low, $high);
+        } else
+        {
+          my $mid = int( ( $low + $high ) / 2 );
+          my @solution_left  = find_max_sub_array_rec( \@a, $low,     $mid );
+          my @solution_right = find_max_sub_array_rec( \@a, $mid + 1, $high );
+          my @solution_cross = find_max_crossing_sub_array( \@a, $low, $mid, $high );
+          my $left_low   = $solution_left[0];
+          my $left_high  = $solution_left[1];
+          my $left_sum   = $solution_left[2];
+          my $right_low  = $solution_right[0];
+          my $right_high = $solution_right[1];
+          my $right_sum  = $solution_right[2];
+          my $cross_low  = $solution_cross[0];
+          my $cross_high = $solution_cross[1];
+          my $cross_sum  = $solution_cross[2];
 
-        if ( $left_sum >= $right_sum and $left_sum >= $cross_sum ) {
-            return @solution_left;
-        }
-        elsif ( $right_sum >= $left_sum and $right_sum > $cross_sum )
-          {
-            return @solution_right;
-        }
-        else {
-            return @solution_cross;
-        }
+          if ( $left_sum >= $right_sum and $left_sum >= $cross_sum ) {
+              return @solution_left;
+          }
+          elsif ( $right_sum >= $left_sum and $right_sum > $cross_sum )
+            {
+              return @solution_right;
+          }
+          else {
+              return @solution_cross;
+          }
+      }
     }
 }
 
--- lib/MaxSum/Squared.pm
+++ lib/MaxSum/Squared.pm
@@ -2,27 +2,30 @@ package MaxSum::Squared;
 
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(find_max_sub_array);
+our @EXPORT_OK = qw(find_max_sub_array find_max_sub_array_part);
 
 sub max_solution {
     my $sum     = shift;
     my $init    = shift;
     my $end     = shift;
     my $max_ref = shift;
-    my @max = @{$max_ref};
+    my @max     = @{$max_ref};
 
     if ( $sum > $max[2] ) {
-        return ($init, $end, $sum);
+        return ( $init, $end, $sum );
     }
 
     return @max;
 }
 
-sub find_max_sub_array {
-    my $i_ref    = shift;
-    my @i        = @{$i_ref};
+sub find_max_sub_array_part {
+    my $i_ref      = shift;
+    my $init_array = shift;
+    my $end_array  = shift;
+
+    my @i = @{$i_ref};
     my @solution = ( 0, 0, 0 );
-    foreach my $init ( 0 .. $#i ) {
+    foreach my $init ( $init_array .. $end_array ) {
         my $sum = 0;
 
         foreach my $end ( $init .. $#i ) {
@@ -32,6 +35,13 @@ sub find_max_sub_array {
     }
 
     return @solution;
+}
+
+sub find_max_sub_array {
+    my $i_ref = shift;
+    my @i     = @{$i_ref};
+
+    return find_max_sub_array_part( $i_ref, 0, $#i );
 }
 
 1;
